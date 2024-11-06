243aaa2ab22a3ea07147d438d52dd402b13ff866
Carsoncox11 <coxp.carson@gmail.com>
2024-11-06 23:19:47 +0000

    Added a createUser scaffolding

--- backend/server.js
+++ backend/server.js
@@ -111,30 +111,56 @@ app.post("/api/login", passport.authenticate('local',{
 }));
 //TODO logout stuff, needs to use req.logout()
 //Need to know if email is also given on login
-// app.post('/api/login', async (req, res, next) => {
-//     // incoming: login, password
-//     // outgoing: id, firstName, lastName, error
-//
-//     var error = '';
-//
-//     const { login, password } = req.body;
-//
-//     const db = dbclient.db();
-//     const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
-//
-//     var id = -1;
-//     var fn = '';
-//     var ln = '';
-//
-//     if (results.length > 0) {
-//         id = results[0].UserId;
-//         fn = results[0].FirstName;
-//         ln = results[0].LastName;
-//     }
-//
-//     var ret = { id: id, firstName: fn, lastName: ln, error: '' };
-//     res.status(200).json(ret);
-// });
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+
+    var error = '';
+
+    const { login, password } = req.body;
+
+    const db = client.db();
+    const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
+
+    var id = -1;
+    var fn = '';
+    var ln = '';
+
+    if (results.length > 0) {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+    }
+
+    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
+    res.status(200).json(ret);
+});
+
+//Need to know if email is also given on login
+app.post('/api/createUser', async (req, res, next) => {
+    // incoming: new login, new password
+    // outgoing: id, firstName, lastName, error
+
+    var error = '';
+
+    const { login, password } = req.body;
+
+    const db = client.db();
+    const results = await db.collection('Users').insertOne({ Login: login, Password: password }).toArray();
+
+    var id = -1;
+    var fn = '';
+    var ln = '';
+
+    if (results.length > 0) {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+    }
+
+    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
+    res.status(200).json(ret);
+});
 
 /* Gerber API calls here for reference
 
