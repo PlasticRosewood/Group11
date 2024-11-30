a6bf9f0e400ef83edddfb7526c9c1e6187cf2d02
Kaevhi <ktotran27@gmail.com>
2024-11-30 17:15:46 -0500

    added popup to voting

--- frontend/src/pages/VotingPage.tsx
+++ frontend/src/pages/VotingPage.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './VotingPage.css';
+import {Link } from 'react-router-dom';
 
 import SideNav from '../components/SideNav';
 
 function VotingPage() {
     const [isExpanded, setIsExpanded] = useState(false); /* side nav shiz */
-    
+    const [showPopup, setShowPopup] = useState(true);
+
+    const [startAnimations, setStartAnimations] = useState(false); /* ensuring animations don't start until yes is clicked */
+
     const createDeck = (deckType: 'left' | 'right') => { /* determining which container to store deck */
 
         return Array(16).fill(true).map((_, index) => { /* for each card */
@@ -15,7 +19,7 @@ function VotingPage() {
             return (
             <div 
                 key={`${deckType}-card-${index}`}
-                className={`card ${deckType}-card`}
+                className={`card ${deckType}-card ${startAnimations ? 'start-animation' : ''}`}
                 style={{
                     zIndex: 16 - index, 
                     marginTop: `${index * 0.8}%`, 
@@ -28,6 +32,11 @@ function VotingPage() {
             </div>
             );
     });
+    };
+
+    const handleYesClick = () => {
+        setShowPopup(false);
+        setStartAnimations(true);
     };
     
     return ( 
@@ -44,7 +53,7 @@ function VotingPage() {
                         </div>
                     </div>
 
-                    <div className="vs"> 
+                    <div className={`vs ${startAnimations ? 'start-animation' :''}`}> 
                         <span className="v">V</span>
                         <span className="s">S</span>
                     </div>
@@ -54,9 +63,24 @@ function VotingPage() {
                         <div className="box-content">
                            
                         </div>
+                    </div> 
+                </div>
+            </div>
+            {showPopup && (
+            <div className="popup">
+                <div className="popup-content">
+                    <div className="pop-text">
+                    Would you like to continue?
+                    </div>
+                    <div className="popup-buttons">
+                        <button onClick={handleYesClick}>Yes</button>
+                        <Link to="/">
+                        <button>No</button>
+                        </Link>
                     </div>
                 </div>
             </div>
+            )}
         </div>
     );
 }
